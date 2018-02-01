4e4a3b6877025d27ec85a7b8dc4f05f195ee7849
BakHb <bakarhaiba@hotmail.fr>
2018-02-01 21:36:41 +0100

    Update to be usable on basic form

--- node/annonce/routes/index.js
+++ node/annonce/routes/index.js
@@ -2,41 +2,63 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {
+        title: 'Express'
+    });
 });
 
 
 /* GET place ad page. */
-router.get('/placeAd', function(req, res, next) {
-  res.render('placeAd', {});
+router.get('/placeAd', function (req, res, next) {
+    res.render('placeAd', {});
 });
 
 /* POST the data of the ad */
-router.post('/addAd', function(req, res) {
-  // Set our internal DB variable
-  var db = req.db;
-  
-  // Get our form values. These rely on the name attributes
-  // Change the values of the form
-  var userName = req.body.userName
-  var adTitle = req.body.adTitle
-  var img = req.body.img
-  
-  // TODO : Change the db name 
-  var collection = db.get('annonce');
-  collection.insert({ 
-	"userName" : userName,
-	"adTitle" : adTitle,
-	"img" : img,
-  }, function (err, doc) {
-  	if (err) {
-		res.send("error");
-	}
-	else {
-		res.redirect("placeAd");
-	}
-  }); 
-}); 
+router.post('/addAd', function (req, res) {
+    // Set our internal DB variable
+    var db = req.db;
+
+    // Get our form values. These rely on the name attributes
+    // Change the values of the form
+    var titre_annonce = req.body.titre_annonce
+    var prix = req.body.prix
+    var vendeur = req.body.vendeur
+    var tel = req.body.tel
+    var model = req.body.model
+    var annee_sortie = req.body.annee_sortie
+    var vitesse = req.body.vitesse
+    var AA = req.body.AA
+    var kmetrage = req.body.kmetrage
+    var img1 = req.body.img1
+    var img2 = req.body.img2
+    var img3 = req.body.img3
+    var infoSup = req.body.infoSup
+    
+
+    // TODO : Change the db name 
+    var collection = db.get('annonce');
+    collection.insert({
+        "titre_annonce": titre_annonce,
+        "prix": prix,
+        "vendeur": vendeur,
+        "tel": tel,
+        "model": model,
+        "annee_sortie": annee_sortie,
+        "vitesse": vitesse,
+        "AA": AA,
+        "kmetrage": kmetrage,
+        "img1": img1,
+        "img2": img2,
+        "img3": img3,
+        "infoSup": infoSup,
+    }, function (err, doc) {
+        if (err) {
+            res.send("error");
+        } else {
+            res.redirect("placeAd");
+        }
+    });
+});
 
 module.exports = router;
